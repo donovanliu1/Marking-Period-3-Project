683dabcaaef382db69f681c2c30f4c7bc7005e39
eric <ericl160@nycstudents.net>
2024-03-15 11:39:42 -0400

    added BG

--- game/core/src/com/mygdx/game/MainMenuScreen.java
+++ game/core/src/com/mygdx/game/MainMenuScreen.java
@@ -2,37 +2,31 @@ package com.mygdx.game;
 
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
-import jdk.javadoc.internal.tool.Start;
-import com.badlogic.gdx.math.Rectangle;
 
-public class MainMenu implements Screen
+public class MainMenuScreen implements Screen
 {
     private final OraclesOdyssey game;
     private OrthographicCamera camera;
-    private Sprite startButton = new Sprite(new Texture(Gdx.files.internal("startbutton2.png")));
-    private int startButtonWidth = (int) startButton.getWidth();
-    private int  startButtonHeight = (int) startButton.getHeight();
+    private Sprite startButtonSprite = new Sprite(new Texture(Gdx.files.internal("startbutton.png")));
+    private Sprite startBackgroundSprite = new Sprite(new Texture(Gdx.files.internal("startbackground.jpg")));
     private int width = Gdx.graphics.getWidth();
     private int height = Gdx.graphics.getHeight();
     private int startX = 0;
     private int startY = 0;
-//    private int startY = (int) (height -startButton.getHeight());
+//    private int startY = (int) (height -startButtonSprite.getHeight());
     private int count = 0;
     private Vector3 touchPoint = new Vector3();
 
 //    private Drawable drawable = new TextureRegionDrawable(new TextureRegion(playTexture);
 //    private ImageButton playButton = new ImageButton(drawable);
 
-    public MainMenu(final OraclesOdyssey gam) {
+    public MainMenuScreen(final OraclesOdyssey gam) {
         game = gam;
         camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         camera.setToOrtho(false, width, height);
@@ -43,7 +37,8 @@ public class MainMenu implements Screen
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin(); // STARTS
-        game.batch.draw(startButton, startX, startY);
+        renderBackground(); // METHOD CREATED TO RENDER BACKGROUND
+        game.batch.draw(startButtonSprite, startX, startY);
 //        game.font.draw(game.batch, "Welcome to Drop!!! ", 100, 150);
 //        game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
         game.batch.end(); // ENDS
@@ -55,24 +50,29 @@ public class MainMenu implements Screen
         if(Gdx.input.isTouched())
         {
             camera.unproject(touchPoint.set(Gdx.input.getX(),Gdx.input.getY(),0));
-//            if (Gdx.input.getX() >= startX && Gdx.input.getX() <= startX + startButton.getWidth() && Gdx.input.getY() >= startY && Gdx.input.getY() <= startY + startButton.getHeight())
+//            if (Gdx.input.getX() >= startX && Gdx.input.getX() <= startX + startButtonSprite.getWidth() && Gdx.input.getY() >= startY && Gdx.input.getY() <= startY + startButtonSprite.getHeight())
 //            {
 //
 //                System.out.println("asdasdasd" + count);
 //                count++;
 //            }
-            if (startButton.getBoundingRectangle().contains(touchPoint.x, touchPoint.y))
+            if (startButtonSprite.getBoundingRectangle().contains(touchPoint.x, touchPoint.y))
             {
                 System.out.println("asdasdasd" + count);
                 System.out.println("touchpointx: " + touchPoint.x);
                 System.out.println("touchpointy: " + touchPoint.y);
                 count++;
+                dispose();
             }
             camera.project(touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0));
         }
 
 //        }
 
+    }
+
+    public void renderBackground() {
+        game.batch.draw(startBackgroundSprite, 0, 0, width, height);
     }
 
     @Override
--- game/core/src/com/mygdx/game/OraclesOdyssey.java
+++ game/core/src/com/mygdx/game/OraclesOdyssey.java
@@ -17,7 +17,7 @@ public class OraclesOdyssey extends Game {
 		batch = new SpriteBatch();
 		// Use LibGDX's default Arial font.
 		font = new BitmapFont();
-		this.setScreen(new MainMenu(this));
+		this.setScreen(new MainMenuScreen(this));
 	}
 
 	public void render()
