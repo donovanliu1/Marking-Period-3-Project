cb3a01277c3db534a3bc3a8049802f58ae7c18f2
donovanliu1 <donovanl28@nycstudents.net>
2024-03-15 15:34:51 -0400

    possibly made start button work

--- game/core/src/com/mygdx/game/MainMenu.java
+++ game/core/src/com/mygdx/game/MainMenu.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 import jdk.javadoc.internal.tool.Start;
@@ -15,19 +16,23 @@ public class MainMenu implements Screen
 {
     private final OraclesOdyssey game;
     private OrthographicCamera camera;
-    private Texture startButton = new Texture(Gdx.files.internal("startbutton2.png"));
+    private Sprite startButton = new Sprite(new Texture(Gdx.files.internal("startbutton2.png")));
+    private int startButtonWidth = (int) startButton.getWidth();
+    private int  startButtonHeight = (int) startButton.getHeight();
     private int width = Gdx.graphics.getWidth();
     private int height = Gdx.graphics.getHeight();
-    private int startX = width/2 - 181;
-    private int startY = height/3;
-    private int startButtonWidth = startButton.getWidth();
-    private int startButtonHeight = startButton.getHeight();
+    private int startX = 0;
+    private int startY = 0;
+//    private int startY = (int) (height -startButton.getHeight());
+    private int count = 0;
+    private Vector3 touchPoint = new Vector3();
+
 //    private Drawable drawable = new TextureRegionDrawable(new TextureRegion(playTexture);
 //    private ImageButton playButton = new ImageButton(drawable);
 
     public MainMenu(final OraclesOdyssey gam) {
         game = gam;
-        camera = new OrthographicCamera();
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         camera.setToOrtho(false, width, height);
     }
 
@@ -48,23 +53,25 @@ public class MainMenu implements Screen
 
         if(Gdx.input.isTouched())
         {
-            System.out.println("\nstartx: " + startX);
-            System.out.println("starty: " + startY);
-            System.out.println("input x: " + Gdx.input.getX());
-            System.out.println("input y: " + Gdx.input.getY());
-            System.out.println("startWidth: " + startButtonHeight);
-            System.out.println("startHeight: " + startButtonHeight);
-
-            if (Gdx.input.getX() >= startX && Gdx.input.getX() <= startX + startButtonWidth && Gdx.input.getY() >= startY && Gdx.input.getY() <= startY + startButtonHeight)
-                System.out.println("asdasdasd");
-            dispose();
+            camera.unproject(touchPoint.set(Gdx.input.getX(),Gdx.input.getY(),0));
+//            if (Gdx.input.getX() >= startX && Gdx.input.getX() <= startX + startButton.getWidth() && Gdx.input.getY() >= startY && Gdx.input.getY() <= startY + startButton.getHeight())
+//            {
+//
+//                System.out.println("asdasdasd" + count);
+//                count++;
+//            }
+            if (startButton.getBoundingRectangle().contains(touchPoint.x, touchPoint.y))
+            {
+                System.out.println("asdasdasd" + count);
+                System.out.println("touchpointx: " + touchPoint.x);
+                System.out.println("touchpointy: " + touchPoint.y);
+                count++;
+            }
+            camera.project(touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0));
         }
-        if (Gdx.input.isTouched()) {
-//            rect.setPosition(Gdx.input.getX(), Gdx.input.getY());
 
-//            game.setScreen(new GameScreen(game));
-//            dispose();
-        }
+//        }
+
     }
 
     @Override
