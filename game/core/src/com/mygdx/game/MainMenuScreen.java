700baf4fe456606447bd529319b4336c7b758ad8
eric <ericl160@nycstudents.net>
2024-03-18 15:29:31 -0400

    overall layout of main menu done. need to fix hitboxes

--- game/core/src/com/mygdx/game/MainMenuScreen.java
+++ game/core/src/com/mygdx/game/MainMenuScreen.java
@@ -17,9 +17,16 @@ public class MainMenuScreen implements Screen
     private Sprite startBackgroundSprite = new Sprite(new Texture(Gdx.files.internal("startbackground.jpg")));
     private int width = Gdx.graphics.getWidth();
     private int height = Gdx.graphics.getHeight();
-    private int startX = 0;
-    private int startY = 0;
-//    private int startY = (int) (height -startButtonSprite.getHeight());
+    private int startButtonX = centerWidth(startButtonSprite);
+    private int startButtonY = centerHeight(startButtonSprite) + height/4;
+    // CHANGE THE BELOW VARIABLES WHEN WE FIND THE SPRITE WE USE
+    private Sprite menuButtonSprite = new Sprite(new Texture(Gdx.files.internal("startbutton.png")));
+    private Sprite creditButtonSprite = new Sprite(new Texture(Gdx.files.internal("startbutton.png"))); // used to cite sources? idk maybe change to like tutorial
+
+    private int menuButtonX = centerWidth(menuButtonSprite) - width/4;
+    private int menuButtonY = centerHeight(menuButtonSprite) - height/4;
+    private int creditButtonX = centerWidth(creditButtonSprite) + width/4;
+    private int creditButtonY = centerHeight(creditButtonSprite) - height/4;
     private int count = 0;
     private Vector3 touchPoint = new Vector3();
 
@@ -38,9 +45,7 @@ public class MainMenuScreen implements Screen
         game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin(); // STARTS
         renderBackground(); // METHOD CREATED TO RENDER BACKGROUND
-        game.batch.draw(startButtonSprite, startX, startY);
-//        game.font.draw(game.batch, "Welcome to Drop!!! ", 100, 150);
-//        game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
+        renderButtons(); // CHECK THE METHOD TO SEE THE RENDERS
         game.batch.end(); // ENDS
 //        Rectangle rect = new Rectangle(0,0,1,1);
 
@@ -50,12 +55,13 @@ public class MainMenuScreen implements Screen
         if(Gdx.input.isTouched())
         {
             camera.unproject(touchPoint.set(Gdx.input.getX(),Gdx.input.getY(),0));
-//            if (Gdx.input.getX() >= startX && Gdx.input.getX() <= startX + startButtonSprite.getWidth() && Gdx.input.getY() >= startY && Gdx.input.getY() <= startY + startButtonSprite.getHeight())
+//            if (Gdx.input.getX() >= startButtonX && Gdx.input.getX() <= startButtonX + startButtonSprite.getWidth() && Gdx.input.getY() >= startButtonY && Gdx.input.getY() <= startButtonY + startButtonSprite.getHeight())
 //            {
 //
 //                System.out.println("asdasdasd" + count);
 //                count++;
 //            }
+            startButtonSprite.getBoundingRectangle();
             if (startButtonSprite.getBoundingRectangle().contains(touchPoint.x, touchPoint.y))
             {
                 System.out.println("asdasdasd" + count);
@@ -66,13 +72,15 @@ public class MainMenuScreen implements Screen
             }
             camera.project(touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0));
         }
-
-//        }
-
     }
 
     public void renderBackground() {
         game.batch.draw(startBackgroundSprite, 0, 0, width, height);
+    }
+    public void renderButtons() {
+        game.batch.draw(startButtonSprite, startButtonX, startButtonY);
+        game.batch.draw(menuButtonSprite, menuButtonX, menuButtonY);
+        game.batch.draw(creditButtonSprite, creditButtonX, creditButtonY);
     }
 
     @Override
@@ -97,5 +105,11 @@ public class MainMenuScreen implements Screen
 
     @Override
     public void dispose() {
+    }
+    public int centerWidth(Sprite sprite) {
+        return (int) ((width - sprite.getWidth())/2);
+    }
+    public int centerHeight(Sprite sprite) {
+        return (int) ((height - sprite.getHeight())/2);
     }
 }
