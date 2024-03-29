5381a07ee58106632e3543ac5e2eaa25fbf04534
donovanliu1 <donovanl28@nycstudents.net>
2024-03-29 12:08:14 -0400

    minor changes - added some getters and setters to Plane.java as well as some instance variables - not sure what else to add also added Collectible.java, which would be used as a superclass for collectibles like ammo and power-ups

--- game/core/src/com/mygdx/game/Plane.java
+++ game/core/src/com/mygdx/game/Plane.java
@@ -3,5 +3,43 @@ package com.mygdx.game;
 public class Plane
 {
     private int ammo;
-    private int max_ammo;
+    private int maxAmmo;
+    private int hp;
+    private int maxHP;
+
+    public Plane(int maxAmmo, int maxHP)
+    {
+        this.maxAmmo = maxAmmo;
+        this.maxHP = maxHP;
+    }
+
+
+
+    public void setMaxAmmo(int max)
+    {
+        maxAmmo = max;
+    }
+
+    public void setMaxHP(int maxHP)
+    {
+        this.maxHP = maxHP;
+    }
+
+    public int getAmmo()
+    {
+        return ammo;
+    }
+
+    public int getMaxAmmo() {
+        return maxAmmo;
+    }
+
+    public int getHp()
+    {
+        return hp;
+    }
+
+    public int getMaxHP() {
+        return maxHP;
+    }
 }
